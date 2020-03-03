900357c8ed28f125cb5a12d941b9b4bf22844860
= <=>
2020-03-03 14:25:51 -0300

    Layout do jogo definido

--- Jogo_da_memoria/Form1.cs
+++ Jogo_da_memoria/Form1.cs
@@ -16,5 +16,15 @@ namespace Jogo_da_memoria
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
